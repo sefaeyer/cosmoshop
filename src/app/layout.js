80feefaf2f2d54597ad502a82a260315beaefcc1
Sefa Eyer <eyersefa@gmail.com>
2024-09-27 20:45:24 +0300

    v1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,4 @@
-import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,8 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body >
+        <h2>HEADER</h2>
         {children}
+        <h3>FOOTER</h3>
       </body>
     </html>
   );
